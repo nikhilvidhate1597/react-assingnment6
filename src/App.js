e0bca2a92dadd9700460097debc2633ff00e7010
nikhilvidhate1597 <102309587+nikhilvidhate1597@users.noreply.github.com>
2022-06-28 18:12:19 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-// import logo from './logo.svg';
 import './App.css';
-
-import {BrowserRouter ,Routes ,Route}from 'react-router-dom'
-import Home from './Component/Home';
-import Student from './Component/Student';
-import Contact from './Component/Contact';
-import NavBar from './Component/NavBar';
-import StudentEdit from './Component/StudentEdit';
-import NewStudent from './Component/NewStudent';
+import Home from './pages/Home';
+import Students from './pages/Students';
+import Contact from './pages/Contact';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Form from './pages/Form';
+import Editform from './pages/Editform';
+import { InputContextProvider } from './Context/inputContext';
 function App() {
   return (
-    <div className="App">
-       <BrowserRouter>
-       <NavBar/>
-       <Routes>
-        <Route path="/home" element ={<Home/>}/>
-        <Route path="/student" element ={<Student/>}/>
-        <Route path="/contact" element ={<Contact/>}/>
-        <Route path="/StudentEdit" element={<StudentEdit/>}/>
-        <Route path="/NewStudents" element={<NewStudent/>}/>
 
-       </Routes>
-       </BrowserRouter>     
-    </div>
+      <InputContextProvider>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/students' element={<Students />}/>
+             <Route path='/form' element={<Form/>}/>
+             <Route path='/edit/:id' element={<Editform/>}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+        </BrowserRouter>
+      </InputContextProvider>
+
   );
 }
-
 export default App;
